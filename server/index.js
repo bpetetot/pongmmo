a8b3aafda804ceca2eb6c2ba5b68fe155a10a539
Fabien JUIF <fabien.juif@zenika.com>
2016-10-08 10:35:32 +0200

    init socket

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const io = require('socket.io')(8000)
+
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  })
+})
